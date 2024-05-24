37e1f32f0b15b7098c449808e5e9349a8e59f685
alexzhang <alexzhangyaotian@gmail.com>
2024-05-24 14:17:51 +0800

    feat:校对功能

--- fe/src/views/El/modes/LCard.vue
+++ fe/src/views/El/modes/LCard.vue
@@ -1,22 +1,27 @@
 <template>
-  <div class="w-full border-2 h-3/4 flex justify-center items-center relative">
+  <div class="w-full h-3/4 flex justify-center items-center relative">
     <div
-      class="w-1/5 h-48 border-2 shadow-xl rounded-md absolute -translate-x-96 translate-y-8 bg-white z-10 flex justify-center items-center cursor-pointer transition-all duration-700"
+      class="w-1/5 h-56 shadow-xl rounded-md absolute -translate-x-96 translate-y-8 bg-gray-50 z-10 flex flex-col justify-center items-center gap-4 cursor-pointer"
       @click="handlePre"
     >
-      {{ props.three[0] }}
+      <div class="font-bold text-xl tracking-wider">
+        {{ props.three[0].en }}
+      </div>
+      <div class="tracking-wider">{{ props.three[0].cn }}</div>
     </div>
 
     <div
-      class="w-1/2 h-96 border-2 shadow-xl rounded-md absolute bg-white z-30 flex justify-center items-center"
+      class="w-2/5 h-80 shadow-xl rounded-lg absolute bg-gray-50 z-30 flex justify-center items-center overflow-hidden"
     >
       <transition
         :enter-active-class="'animate__animated animate__fadeInDown'"
         :leave-active-class="'animate__animated animate__fadeOutDown'"
         mode="out-in"
+        
       >
-        <div :key="props.three[1]" class="border-2 w-full h-full">
-          <div class="p-2 m-2 border-2 flex items-center">
+        <div :key="props.three[1]" class="w-full h-full">
+          <!-- 提示区域 -->
+          <div class="p-2 m-2 flex items-center">
             <div @click="toggle" class="cursor-pointer mr-8">
               <img
                 class="w-8"
@@ -33,20 +38,31 @@
               </div>
             </transition>
           </div>
-
-          <div class="border-2">
-            <input type="text">
-            {{ props.three[1] }}
+          <!-- 输入区域 -->
+          <div class="h-3/4 flex flex-col justify-center items-center">
+            <input
+              class="w-1/2 border-b-2 h-12 outline-none text-xl indent-6 bg-gray-50 tracking-wider"
+              type="text"
+              ref="inputArea"
+              v-model="inputValue"
+              v-focus
+              @keyup.enter="handleEnter"
+              @keyup.up="toggle"
+            />
+            <div class="pt-5 tracking-wider">{{ props.three[1].cn }}</div>
           </div>
         </div>
       </transition>
     </div>
 
     <div
-      class="w-1/5 h-48 border-2 shadow-xl rounded-md absolute translate-x-96 translate-y-8 bg-white z-10 flex justify-center items-center cursor-pointer"
+      class="w-1/5 h-56 shadow-xl rounded-md absolute translate-x-96 translate-y-8 bg-gray-50 z-10 flex flex-col justify-center items-center gap-4 cursor-pointer"
       @click="handleNext"
     >
-      {{ props.three[2] }}
+      <div class="font-bold text-xl tracking-wider">
+        {{ props.three[2].en }}
+      </div>
+      <div class="tracking-wider">{{ props.three[2].cn }}</div>
     </div>
   </div>
 </template>
@@ -57,17 +73,45 @@ import { ref } from "vue";
 const props = defineProps(["three"]);
 const emits = defineEmits(["pre", "next"]);
 const isActive = ref(false);
+const inputValue = ref("");
+const inputArea = ref(null);
 
 const handlePre = () => {
+  reset();
   emits("pre");
 };
 const handleNext = () => {
+  reset();
   emits("next");
 };
 
 // 切换展示
 const toggle = () => {
   isActive.value = !isActive.value;
+  inputArea.value.focus();
+};
+
+// 自定义指令
+const vFocus = {
+  mounted: (el) => el.focus(),
+};
+
+// reset状态
+const reset = () => {
+  // 重置
+  inputValue.value = "";
+  isActive.value = false;
+};
+
+// 确认输入
+const handleEnter = () => {
+  const origin = props.three[1].en.trim();
+  const value = inputValue.value.trim();
+  if (value === origin) {
+    handleNext();
+  } else {
+    reset();
+  }
 };
 </script>
 <style></style>
